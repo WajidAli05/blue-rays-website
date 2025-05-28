116947cc8d7b75601acf89fd1ea727032447cb15
WajidAli05 <wajidalise@gmail.com>
2025-05-28 11:29:18 -0700

    Routing implemented using React Router@7.6.1

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
 import { SignupForm } from "./components/SignupForm"
+import { Routes, Route } from 'react-router'
 
 function App() {
 
   return (
     <main className="min-h-screen bg-gray-50 dark:bg-black flex items-center justify-center">
-      <SignupForm />
+      <Routes>
+        <Route path='/signup' element={<SignupForm />} />
+      </Routes>
     </main>
   )
 }
