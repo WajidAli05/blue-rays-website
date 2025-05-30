e885927bc6b759a00e82be3cd17ea17b9bdf30b1
WajidAli05 <wajidalise@gmail.com>
2025-05-30 10:07:10 -0700

    unified backend for all types of users | signin with google implemeted

--- Backend/routes/v1/affiliateProgramRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-const router = express.Router();
-import { getAffiliatePrograms } from '../../controllers/affiliateProgramController.mjs';
-import { validateToken } from '../../middlewares/accessTokenHandler.js';
-
-router.get('/affiliate-program' , validateToken, getAffiliatePrograms);
-
-export default router;
--- Backend/routes/v1/fileTypeRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-const router = express.Router();
-import { getFileTypes } from '../../controllers/fileTypeController.mjs';
-import { validateToken } from '../../middlewares/accessTokenHandler.js';
-
-router.get('/file-types', validateToken, getFileTypes);
-
-export default router;
